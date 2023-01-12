7cedcfcac0508839049fa4d7d11b7629a45ea01a
miguel.galera <miguel.galera@DESKTOP-9MH23G4.mshome.net>
2023-01-12 12:23:25 +0100

    FUNCIONES

---
+++ src/ejercicio01/Bicicleta.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio01;
+
+/**
+ *
+ * @author miguel.galera
+ */
+public class Bicicleta {
+    private double revoluciones;
+    private int piñonActual;
+    
+    public Bicicleta(){
+    
+}
+    public Bicicleta(double revoluciones, int piñonActual){
+        this.revoluciones=0;
+        this.piñonActual=3;
+    }
+
+    public double getRevoluciones() {
+        return revoluciones;
+    }
+
+    public void setRevoluciones(double revoluciones) {
+        this.revoluciones = revoluciones;
+    }
+
+    public int getPiñonActual() {
+        return piñonActual;
+    }
+
+    public void setPiñonActual(int piñonActual) {
+        this.piñonActual = piñonActual;
+    }
+    
+    //********* METODOS ********
+    public void pedalear(){
+        if(this.revoluciones==0){
+            this.revoluciones=1;
+        }else{
+            this.revoluciones=this.revoluciones*2;
+        }
+
+    }
+    public void frenar(){
+        if(this.revoluciones==1){
+            this.revoluciones=0;
+        }else{
+            this.revoluciones=this.revoluciones/2;           
+        }  
+    }
+    public void cambiarPiñon(int piñon){
+        if(piñon>=0 && piñon<=5){
+            this.piñonActual=piñon;
+        }else{
+            System.out.println("ERROR. Piñón incorrecto");
+        }
+    }
+    
+}
---
+++ src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio01;
+
+/**
+ *
+ * @author miguel.galera
+ */
+public class Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       Bicicleta miBicicleta=new Bicicleta();
+    }
+    
+}
